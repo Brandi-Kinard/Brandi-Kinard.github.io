086c5fe9243071aa97c5e3b2b3b498d9f304fa5b
Kinard <bkinard@38f9d35ea377.ant.amazon.com>
2019-07-10 14:20:33 -0700

    Initial commit

---
+++ myfirstjs.js
@@ -0,0 +1,7 @@
+// function myFunction() {
+// 	alert("I am an alert box!")
+// }
+
+function replaceImage() {
+	document.getElementById("selfie").src="https://66.media.tumblr.com/df7776c88e6bf4bff2e5f132988d5bef/tumblr_o68lcpXzZS1r8t1t8o2_540.jpg"
+}

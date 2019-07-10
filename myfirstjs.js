08ca1bfb9c0f69b6b49ed7884f83acaed7b7fc7d
Kinard <bkinard@38f9d35ea377.ant.amazon.com>
2019-07-10 15:58:53 -0700

    Initial commit

--- myfirstjs.js
+++ myfirstjs.js
@@ -5,3 +5,8 @@
 function replaceImage() {
 	document.getElementById("selfie").src="https://66.media.tumblr.com/df7776c88e6bf4bff2e5f132988d5bef/tumblr_o68lcpXzZS1r8t1t8o2_540.jpg"
 }
+
+function showHiddenMessage() {
+	document.getElementById("hiddenMessage").innerHTML = "This is my secret...";
+}
+
